51ceb006bbbd95b0e8ed44397f74b7b62cea1bce
qunoot zehra <qunootzehra21@gmail.com>
2026-04-09 19:54:17 +0500

    somechanges in database

--- salon_management.sql
+++ salon_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2026 at 02:04 PM
+-- Generation Time: Apr 09, 2026 at 01:49 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
